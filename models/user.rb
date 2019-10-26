aab38442e39e23bc6eb2e87c8dc95f835e7d11c7
Randel Jiao <randeljiao@gmail.com>
2019-10-26 23:44:50 +0000

    Done.

--- models/user.rb
+++ models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   def say_name
     "my name is #{self.name}"
   end
+  
+  user = User.new
+user.name = "Beth"
+user.email = "beth@beth.com"
+user.fav_icecream = "rocky road"
+user.save
+
+user = User.first
+user.name = "Trisha Yearwood"
+user.save
+
+user = User.first
+user.delete
+
+user = User.find_by_id(2)
+user = User.find_by(:name => "Beth")
+user = User.first
+user = User.last
 end
