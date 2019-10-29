1ca3f0535059db160e3b99b478c94de43367d841
yasmine-hj <yasminehajaji95@gmail.com>
2019-10-29 01:01:32 +0000

    Done.

--- db/migrate/20151117223101_create_users.rb
+++ db/migrate/20151117223101_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+
     create_table :users do |t|
       t.string :name
       t.string :email
       t.string :fav_icecream
+      
     end
   end
 end
+
